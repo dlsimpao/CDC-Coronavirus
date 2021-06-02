753ddf28c3ca36e3af44d455daea2d473522fe3e
Ricardo Simpao <19231567+dlsimpao@users.noreply.github.com>
2021-06-01 20:48:06 -0700

    Update ui.R

--- ui.R
+++ ui.R
@@ -13,22 +13,46 @@ library(shiny)
 shinyUI(fluidPage(
 
     # Application title
-    titlePanel("CDC Application Name"),
+    titlePanel("COVID-19 Preliminary Diagnosis"),
+    
 
     # Sidebar with a slider input for number of bins
     sidebarLayout(
         sidebarPanel(
-            checkboxGroupInput("symptoms", "Select all that apply:",  c("Abnormal Chest X-ray","Acute Respiratory Distress Syndrome",
-                                              "Fever > 100.4F", "Felt Feverish","Chills","Muscle aches","Runny nose", 
-                                              "Sore throat","Cough","Shortness of breath","Nausea or vomitting","Headache",
-                                              "Abdominal pain","Diarrhea","Pre-existing medical conditions?")),
+            checkboxGroupInput("symptoms", "Select all that apply:",  c("Healthcare worker",
+                                                                        "Pneumonia",
+                                                                        "Abnormal chest x-ray",
+                                                                        "Acute Respiratory Distress",
+                                                                        "Fever",
+                                                                        "Severe Fever",
+                                                                        "Chills",
+                                                                        "Muscle aches (Mylagia)",
+                                                                        "Runny nose",
+                                                                        "Sore Throat",
+                                                                        "Cough",
+                                                                        "Shortness of breath",
+                                                                        "Nausea/Vomitting",
+                                                                        "Headache",
+                                                                        "Abdominal pain",
+                                                                        "Diarrhea",
+                                                                        "Pre Existing Medical Conditions")),
+            selectInput("gender", "Select gender", choices = c("Male", "Female")),
+            selectInput("age", "Select age group", choices = c("1-9","10-19","20-29","30-39","40-49","50-59","60-69","70-79","80+")),
+            selectInput("test", "Select one", choices = c("Tested but no results yet",
+                                                          "Tested with positive results")),
             actionButton("diagnose","Apply")
         ),
         
 
         # Show a plot of the generated distribution
         mainPanel(
-            tableOutput("tbl")
+            tableOutput("tbl"),
+            br(),
+            helpText("How to Interpret:"),
+            helpText("Hospital Yes (0.989) Given your symptoms, you have a 98.9% probability of needing to visit the hospital."),
+            helpText("ICU No (0.899) Given your symptoms, you have a 89.9% probability of not needing the ICU."),
+            tableOutput("tbl2"),
+            tableOutput("tbl3")
         )
     )
 ))
